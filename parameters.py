340a1d6b56c6d77e8e48781400a54f088e2d0764
Johannes Voll Kolstø <johannesvk@protonmail.com>
2020-11-02 14:13:26 +0100

    Minor differences.

--- parameters.py
+++ parameters.py
@@ -14,7 +14,7 @@ T_C, Cc, a_sc, r_sc, a_lc, r_lc = sy.symbols("T_C, Cc, a_sc, r_sc, a_lc, r_lc",
                                             real=True, positive=True)
 
 # Earth variables:
-T_E, r_se, r_le, eps_E = sy.symbols("T_E, r_se, r_le, eps_E",
+T_E, a_se, r_se, a_le, r_le, eps_E = sy.symbols("T_E, a_se, r_se, a_le, r_le, eps_E",
                                     real=True, positive=True)
 
 # Effective Black body emission power for atmosphere, clouds and the eart:
@@ -22,6 +22,9 @@ tau_A, tau_C, tau_E = eps_A*sigma*T_A**4, eps_A*sigma*T_C**4, sigma*T_E**4
 
 # Useful variables for defining our model:
 a_sa = sy.Integer(1) - (sy.Integer(1) - a_sw)*(sy.Integer(1) - a_O3)
+a_la = a_sw
+r_la = sy.Integer(0)
+
 geometric_reflection = sy.Integer(1)/(sy.Integer(1) - r_sc*r_sm)
 
 base_param_values = [(sigma, 5.670374e-8), (P_sun, 341.3)]
@@ -30,7 +33,7 @@ atmosphere_param_values = [(r_sm, 0.1065), (a_sw, 0.1451), (a_lw, 0.8258), (a_O3
 
 cloud_param_values = [(Cc, 0.66), (a_sc, 0.1239), (r_sc, 0.22), (a_lc, 0.622), (r_lc, 0.195)]
 
-earth_param_values = [(r_se, 0.17), (r_le, 0.0), (eps_E, 1.0)]
+earth_param_values = [(a_se, 1.0), (r_se, 0.17), (a_le, 1.0), (r_le, 0.0), (eps_E, 1.0)]
 
 parameter_values = base_param_values + atmosphere_param_values + \
                 cloud_param_values + earth_param_values
